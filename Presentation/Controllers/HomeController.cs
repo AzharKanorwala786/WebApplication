2713c3d24fa370a8d35682b2db0eadce66d56f92
Azhar Kanorwala <azharkanorwala52@outlook.com>
2017-07-09 23:01:31 +0530

    Partial Working Solution

--- Presentation/Controllers/HomeController.cs
+++ Presentation/Controllers/HomeController.cs
@@ -16,23 +16,27 @@ namespace Presentation.Controllers
         {
             ProductVM model = new ProductVM();
 
-            List<ProductVM> products = new List<ProductVM>();
-
-            model.Name = "Amazon Echo";
-            model.Description = "The Echo is a Bluetooth speaker powered by Alexa";
-            model.Price = 350;
-            model.Image = "product_one.jpg";
-            model.CategoryId = 1;
-            model.Category = "Electronics";
-
-            products.Add(model);
-
-            return PartialView("Products", model);
+            List<ProductVM> products = new List<ProductVM>()
+            {
+                new  ProductVM() { Name = "Amazon Echo",Description = "The Echo is a Bluetooth speaker powered by Alexa",Price = 350,CategoryId = 1,Category = "Electronics"},
+                new  ProductVM() { Name = "Amazon Echo",Description = "The Echo is a Bluetooth speaker powered by Alexa",Price = 350,CategoryId = 1,Category = "Electronics"},
+            };
+            //model.Name = "Amazon Echo";
+            //model.Description = "The Echo is a Bluetooth speaker powered by Alexa";
+            //model.Price = 350;
+            //model.Image = "product_one.jpg";
+            //model.CategoryId = 1;
+            //model.Category = "Electronics";
+            //products.Add(model); 
+            return View(products);
         }
-
         public ActionResult Add()
         {
             return PartialView();
+        }
+        public ViewResult Products()
+        {
+            return View("Products");
         }
     }
 }
